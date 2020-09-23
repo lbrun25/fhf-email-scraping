3adda54ac0bf85da6689cfe55e4b498a4ddaf60c
Lucien Brun <lucien.brun@epitech.eu>
2020-09-23 17:14:43 +0200

    Add upper and lower arguments

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+import time
+import sys
+import argparse
 
 
 website_url = "https://etablissements.fhf.fr"
 directory_endpoint = "annuaire/"
+
+
+class Config:
+    mode: int
+    startIndex: int
+    endIndex: int
+    sleepTime: float
+    isRetry: bool
+
+
+config = Config()
 
 
 def get_soup(url):
@@ -65,26 +79,80 @@ def do_nominations_pages(fiches, emails):
     fiches = two_dimensional_array_to_list(fiches)
     fiches = list(dict.fromkeys(fiches))
     emails.append(get_emails(fiches))
+    emails = two_dimensional_array_to_list(emails)
 
 
 def write_output(emails):
     # Remove duplicate
-    emails = two_dimensional_array_to_list(emails)
-    print(emails)
     emails = list(dict.fromkeys(emails))
 
     # Write in output.txt
     f = open("output.txt", "a")
     f.seek(0)
     f.truncate()
-    f.write("\n".join(emails))
+    f.write("\n".join(emails).lower())
     f.close()
+
+
+# Do fiches https://etablissements.fhf.fr/annuaire/hopital-fiche.php?id=2
+def do_fiches(emails):
+    i = config.startIndex
+    notExistMsg = "Cet établissement n'existe pas ou n'existe plus."
+    retry = 0
+
+    while i < config.endIndex:
+        fiche = "hopital-fiche.php?id={}".format(i)
+        url = "{}/{}{}".format(website_url, directory_endpoint, fiche)
+        try:
+            print(url)
+            soup = get_soup(url)
+            result = soup.find("h1", attrs={"class":"intro_article"})
+
+            if result.text.strip() != notExistMsg:
+                mail = soup.find('span', attrs={'class':'courriel'})
+                if mail is not None:
+                    output = mail.text.split()[-1]
+                    emails.append(output.strip())
+        except Exception as e:
+            print(e)
+            if retry < 3 and config.isRetry:
+                retry += 1
+                time.sleep(10)
+                continue
+        i += 1
+        retry = 0
+        time.sleep(config.sleepTime)
 
 
 def main():
     fiches = []
     emails = []
-    do_nominations_pages(fiches, emails)
+    parser = argparse.ArgumentParser(description="Get some mail on https://etablissements.fhf.fr")
+    parser.add_argument("--mode", required=True, help="1. Get emails from the nominations page\n2. Get emails by iterating fiche id", type=int)
+    parser.add_argument("--lower", required=False, help="Start index", type=int)
+    parser.add_argument("--upper", required=False, help="End index", type=int)
+    parser.add_argument("--sleep", required=False, help="Time to sleep between each request", type=float)
+    parser.add_argument("--retry", required=False, help="Retry request if it failed", type=bool)
+
+
+    args = parser.parse_args()
+    config.mode = args.mode
+    config.startIndex = args.lower
+    config.endIndex = args.upper
+    config.sleepTime = args.sleep
+    config.isRetry = args.retry
+
+    if config.mode == 1:
+        do_nominations_pages(fiches, emails)
+    elif config.mode == 2:
+        if config.startIndex is None or config.endIndex is None:
+            print("Please specify lower and upper bound to iterate fiche id pages.")
+            sys.exit(1)
+        else:
+            do_fiches(emails)
+    else:
+        print("Unknown mode. Please select 1 or 2")
+        sys.exit(1)
     write_output(emails)
 
 
